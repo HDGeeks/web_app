c993f24f90c8c6995139845113d574a1cdbc4820
Daniel Tesfai <dannyhd88@gmail.com>
2024-10-06 11:57:11 +0200

    changed root /home

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from  flask import Flask,send_from_directory
 app = Flask(__name__, static_folder='static')
 
 
-@app.route("/")
+@app.route("/home")
 def my_file(filename="index.html"):
     return send_from_directory(app.static_folder,filename)
 
